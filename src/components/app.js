89dbff315098c734890f9cbfcaefbdf6b3adbb81
RickySmith22 <rickysmith22@gmail.com>
2022-03-01 12:27:10 -0800

    added routes for homepage and about-me

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react';
 import moment from 'moment';
+import {  BrowserRouter as Router, Switch, Route  } from "react-router-dom";
 
-import PortfolioContainer from "./portfolio/portfolio-container"
+import PortfolioContainer from "./portfolio/portfolio-container";
+import NavigationContainer from "./navigation/navigation-container";
+import Home from "./navigation/pages/home";
+import About from "./navigation/pages/about";
 
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
+        <Router>
+          <div>
+            <NavigationContainer />
+
+            <Switch>
+              <Route exact path="/" component={Home} />         
+              <Route path="/about-me" component={About} />         
+            </Switch>
+          </div>
+        </Router>
         <h1>Ricky's Portfolio Project</h1>
         <div>{moment().format('MMMM Do YYYY, h:mm:ss a')}</div>
         <PortfolioContainer />
--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -8,12 +8,25 @@ export default class PortfolioContainer extends Component {
 
       this.state = {
           pageTitle: "Welcome to Ricky's Portfolio",
+          isLoading: false,
           data: [
-              {title: "McDonalds"},
-              {title: "Team"},
-              {title: "USMC"}
+              {title: "McDonalds", catagory: "ff"},
+              {title: "Team", catagory: "delivery"},
+              {title: "USMC", catagory: "military"},
+              {title: "Nike", catagory: "retail"},
+              {title: "Polaris", catagory: "delivery"}
             ]
       };
+
+      this.handleFilter = this.handleFilter.bind(this);
+    }
+
+    handleFilter(filter) {
+        this.setState({
+            data: this.state.data.filter(item => {
+                return item.catagory === filter;
+            })
+        });
     }
 
     portfolioItems() {
@@ -21,12 +34,33 @@ export default class PortfolioContainer extends Component {
             return <PortfolioItem title={item.title} url={"google.com"} />;
         });
     }
+
+
+
     render() {
+      if (this.state.isLoading) {
+          return <div>Loading...</div>
+      }
+
       return(
           <div>
               <h2>{this.state.pageTitle}</h2>
 
+              <button onClick={() => this.handleFilter('ff')}>
+                  Fast Food
+              </button>
+              <button onClick={() => this.handleFilter('delivery')}>
+                  Delivery
+              </button>
+              <button onClick={() => this.handleFilter('military')}>
+                  Military
+              </button>
+              <button onClick={() => this.handleFilter('retail')}>
+                  Retail
+              </button>
+
              {this.portfolioItems()}
+
           </div>
       )
     }
